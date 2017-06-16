c80ec1972326d3f7c6ddbf255e3a1ecaddf3666b
puppetmaster3 <vicmasons@gmail.com>
2017-06-16 18:57:45 -0400

    dbind should work

--- srv/scode/route/two/index.js
+++ srv/scode/route/two/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const pug = require('pug')
-const transformer = require('jstransformer')
-const dot = transformer (require('jstransformer-dot'))
+const doT = require('dot')
+
+const cheerio = require('cheerio')
 
 const BLX = require('./BLX')
 
 const Util = require('topseed-util')
-const U = new Util() 
+const U = new Util()
+ 
 var options = {}
 options.pretty = true
 
@@ -18,16 +20,39 @@ router.get('/', function (req, res) {
 	console.log('ssr')
 
 	const requestedResource = ROOT + '/page/two/indexM.pug'
-	const html = pug.renderFile(requestedResource, options)
+	var values = [
+			{
+			url: 'http://www.yahoo.com',
+			head_line: 'Yahoo'
+			},
+			{
+			url: 'http://mail.google.com',
+			head_line: 'Gmail'
+			},
+			{
+			url: 'https://www.cnet.com/news/',
+			head_line: 'News'
+			}
+		]
+
 	// SSR, you get data first. SPA, you display view first, like a 'blank' component. Early binding, vs late binding
-	var d = dot.render('Hello, {{=it.name}}!', { name: 'World'}).body
 
-
-	res.status(200).send(d)
+	const h = pug.renderFile(requestedResource, options)
+	const $ = cheerio.load(h) // load in the HTML into cheerio
+	
+	const tpl1 = $('#Lst1Tpl').text()
+	const html = dBind(tpl1, values)
+	console.log(html)
+	
+	res.status(200).send(h)
 
 })//get
 
-
+function dBind (tpl, data) { // take tmpl and bind w/ data
+	console.log(tpl, data)
+	const tpl1Foo = doT.template(tpl)
+	return tpl1Foo(data)
+}
 
 //###################### 
 module.exports = router
