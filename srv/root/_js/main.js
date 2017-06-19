b676b54d98dcb9f15eb4ac94bc2591aa33468257
puppetmaster3 <vicmasons@gmail.com>
2017-06-19 18:47:02 -0400

    Patched

--- srv/root/_js/main.js
+++ srv/root/_js/main.js
@@ -3,15 +3,7 @@ console.log('main')
 
 loadjs.ready(['polyfills', 'keyLibs'], {// loaded setup libs
 	success: function(){
-
-		loadjs([
-
-
-
-			], { success: function(){
-				TS.signalAppReady()
-			}
-		})//loadjs
+		TS.signalAppReady()
 	}//suc
 })
 
